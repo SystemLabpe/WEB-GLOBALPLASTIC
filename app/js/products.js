dd24b5fc94bf9fc169b715aeacb962c033bfe2f8
JuanMosqueraR <juan.mosquera@systemlab.pe>
2020-09-07 22:43:29 -0500

    fix load products

--- app/js/products.js
+++ app/js/products.js
@@ -5,54 +5,58 @@
   /*  =========================================================================
    Products gallery
   ========================================================================== */
-  var $container = $('.products-gallery');
 
-  $container.isotope({
-    // options
-    itemSelector: '.product-item',
-    layoutMode: 'fitRows',
-    getSortData: {
-      capacity: function(itemElem) {
-        // console.log($(itemElem));
-        // console.log($(itemElem).attr('data-category'));
-        // if ($(itemElem).hasAttr('data-category')) {
-        if (typeof $(itemElem) !== typeof undefined && $(itemElem) !== false) {
-          var capacity = $(itemElem).attr('data-category');
-          console.log('capacity => ', capacity);
-          return parseInt(capacity);
-        }
-      }
-    }
-  });
-
-  var filters = {};
-
-  $('#products-filter li a').on('click', function() {
-
-    var $filter = $( event.currentTarget );
-    // get group key
-    var $categoryGroup = $filter.parents('.categories-list');
-    var filterGroup = $categoryGroup.attr('data-filter-group');
-
-    filters[ filterGroup ] = $filter.attr('data-filter');
-
-    var filterValue = concatValues(filters);
+  //Hide Loading Box (Preloader)
+  function setupFilter() {
+    var $container = $('.products-gallery');
 
     $container.isotope({
-      filter: filterValue,
-      sortBy: 'capacity'
+      // options
+      itemSelector: '.product-item',
+      layoutMode: 'fitRows',
+      getSortData: {
+        capacity: function(itemElem) {
+          // console.log($(itemElem));
+          // console.log($(itemElem).attr('data-category'));
+          // if ($(itemElem).hasAttr('data-category')) {
+          if (typeof $(itemElem) !== typeof undefined && $(itemElem) !== false) {
+            var capacity = $(itemElem).attr('data-category');
+            console.log('capacity => ', capacity);
+            return parseInt(capacity);
+          }
+        }
+      }
     });
-  });
 
-  // change is-checked class on buttons
-  $('.categories-list').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'a', function( event ) {
-      $buttonGroup.find('.is-checked').removeClass('is-checked');
-      var $button = $( event.currentTarget );
-      $button.addClass('is-checked');
+    var filters = {};
+
+    $('#products-filter li a').on('click', function() {
+
+      var $filter = $( event.currentTarget );
+      // get group key
+      var $categoryGroup = $filter.parents('.categories-list');
+      var filterGroup = $categoryGroup.attr('data-filter-group');
+
+      filters[ filterGroup ] = $filter.attr('data-filter');
+
+      var filterValue = concatValues(filters);
+
+      $container.isotope({
+        filter: filterValue,
+        sortBy: 'capacity'
+      });
     });
-  });
+
+    // change is-checked class on buttons
+    $('.categories-list').each( function( i, buttonGroup ) {
+      var $buttonGroup = $( buttonGroup );
+      $buttonGroup.on( 'click', 'a', function( event ) {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        var $button = $( event.currentTarget );
+        $button.addClass('is-checked');
+      });
+    });
+  }
 
   function concatValues( obj ) {
     var value = '';
@@ -61,5 +65,9 @@
     }
     return value;
   }
+
+  $(window).on('load', function() {
+    setupFilter();
+  });
 
  })(window.jQuery);
