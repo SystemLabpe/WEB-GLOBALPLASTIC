49480821f8e8443f52ad7fc41d123d4a3ac9f009
coffmkratsystemlab <josue.barrios@systemlab.pe>
2020-10-26 14:10:13 -0500

    Update send email

--- app/sendemail.php
+++ app/sendemail.php
@@ -1,10 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "WEB - GLOBALPLASTIC" );
+define( "RECIPIENT_NAME", "John Doe" );
 define( "RECIPIENT_EMAIL", "josuebarriosm@gmail.com" );
-// define( "RECIPIENT_EMAIL", "ventas@globalplastic.pe" );
-
 
 
 // Read the form values
@@ -12,23 +10,25 @@ $success = false;
 $userName = isset( $_POST['username'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['username'] ) : "";
 $senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
 $userPhone = isset( $_POST['phone'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
-//$userSubject = isset( $_POST['subject'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
+$product = isset( $_POST['product'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['product'] ) : "";
+$productQty = isset( $_POST['product-qty'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['product-qty'] ) : "";
 $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
 
 // If all values exist, send the email
-if ( $userName && $senderEmail && $userPhone /*&& $userSubject*/ && $message) {
+if ( $userName && $senderEmail && $userPhone) {
   $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
   $headers = "From: " . $userName . "";
-  $msgBody = " Name: ". $userName . " Email: ". $senderEmail . " Phone: ". $userPhone . " Subject: ". $userName . " Message: " . $message . "";
+  $msgBody = " Name: ". $userName . " Email: ". $senderEmail . " Phone: ". $userPhone . " Product: ". $product ." Product-qty: ". $productQty . " Message: " . $message . "";
   $success = mail( $recipient, $headers, $msgBody );
 
-  // Set Location After Successsfull Submission
+  //Set Location After Successsfull Submission
   // header('Location: contacto.html?message=Successfull');
+  header('Location: contacto.html?message='.$success);
 }
 
 else{
 	//Set Location After Unsuccesssfull Submission
-  	// header('Location: contacto.html?message=Failed');
+  	header('Location: contacto.html?message=Failed');	
 }
 
 ?>
